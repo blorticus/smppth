b818b06da5ab878475f667e57d437542e72f4bb4
blorticus <boguese@hotmail.com>
2019-07-11 01:56:08 -0700

    Broken out most classes into separate files

--- src/esme/esme-cluster.go
+++ src/esme/esme-cluster.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net"
+	"os"
+	"path"
 	"smpp"
 )
 
@@ -11,67 +15,21 @@ const (
 	receivedMessage esmeEventType = iota
 )
 
-type smppBindInfo struct {
-	remoteIP   net.IP
-	remotePort uint16
-	systemID   string
-	password   string
-	systemType string
-}
-
 type messageDescriptor struct {
+	sendFromEsmeNamed string
+	sendToSmscNamed   string
+	pdu               *smpp.PDU
 }
 
 type esmeListenerEvent struct {
-	Type     esmeEventType
-	smppPDU  *smpp.PDU
-	peerIP   net.IP
-	peerPort uint16
+	Type       esmeEventType
+	sourceEsme *esme
+	smppPDU    *smpp.PDU
+	peerIP     net.IP
+	peerPort   uint16
 }
 
-type esme struct {
-	bindIP   net.IP
-	bindPort uint16
-}
-
-func (esme *esme) performTranceiverBindTo(bindInfo *smppBindInfo) error {
-	return nil
-}
-
-func (esme *esme) startListening() {
-}
-
-func (esme *esme) sendMessageToPeer(message *messageDescriptor) {
-
-}
-
-func (esme *esme) incomingEventChannel() <-chan *esmeListenerEvent {
-	return nil
-}
-
-type esmeDefinitionYamlReader struct {
-}
-
-func newEsmeDefinitionYamlReader() *esmeDefinitionYamlReader {
-	return &esmeDefinitionYamlReader{}
-}
-
-func (reader *esmeDefinitionYamlReader) parseFile(fileName string) (*esme, []*smppBindInfo, error) {
-	return nil, nil, nil
-}
-
-type brokerForMessagesToSend struct {
-}
-
-func newBrokerForMessagesToSend() *brokerForMessagesToSend {
-	return &brokerForMessagesToSend{}
-}
-
-func (broker *brokerForMessagesToSend) retrieveSendMessageChannel() <-chan *messageDescriptor {
-	return nil
-}
-
-func (broker *brokerForMessagesToSend) startListening() {
+type esmeDescriptor struct {
 }
 
 type outputter struct {
@@ -86,49 +44,82 @@ func (outputter *outputter) sayThatMessageWasReceived(message *smpp.PDU, peerIP 
 }
 
 func (outputter *outputter) dieIfError(err error) {
-
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
 
 // esme-cluster <yaml_file>
 //
 func main() {
+	app := newEsmeClusterApplication()
 	outputter := newOutputter()
 
-	yamlFileName, err := parseCommandLine()
+	yamlFileName, err := app.parseCommandLine()
 	outputter.dieIfError(err)
 
-	yamlReader := newEsmeDefinitionYamlReader()
+	yamlReader := newApplicationConfigYamlReader()
 
-	thisEsme, smscRemoteBinds, err := yamlReader.parseFile(yamlFileName)
+	esmes, _, err := yamlReader.parseFile(yamlFileName)
 	outputter.dieIfError(err)
 
-	broker := newBrokerForMessagesToSend()
-	channelOfMessagesToSend := broker.retrieveSendMessageChannel()
-	go broker.startListening()
+	esmeEventListenerConcentrator := newEventChannelConcentrator()
 
-	for _, smsc := range smscRemoteBinds {
-		err := thisEsme.performTranceiverBindTo(smsc)
-		outputter.dieIfError(err)
+	for _, esme := range esmes {
+		app.mapEsmeNameToItsReceiverChannel(esme.name, esme.outgoingMessageChannel())
+		esmeEventListenerConcentrator.addEventChannelForEsmeNamed(esme.name, esme.incomingEventChannel())
+		go esme.startListening()
 	}
 
-	go thisEsme.startListening()
+	appControlBroker := newApplicationControlBroker()
+	channelOfMessagesToSend := appControlBroker.retrieveSendMessageChannel()
+	go appControlBroker.startListening()
 
-	esmeEventChannel := thisEsme.incomingEventChannel()
+	esmeEventListenerConcentrator.startReceivingEvents()
+	eventConcentratorChannel := esmeEventListenerConcentrator.retrieveConcentrationChannel()
 
 	for {
 		select {
-		case messageDescriptor := <-channelOfMessagesToSend:
-			thisEsme.sendMessageToPeer(messageDescriptor)
-
-		case event := <-esmeEventChannel:
-			switch event.Type {
-			case receivedMessage:
+		case event := <-eventConcentratorChannel:
+			if event.Type == receivedMessage {
 				outputter.sayThatMessageWasReceived(event.smppPDU, event.peerIP, event.peerPort)
 			}
+
+		case messageToSendDescriptor := <-channelOfMessagesToSend:
+			sendChannel := app.retrieveOutgoingMessageChannelFromEsmeNamed(messageToSendDescriptor.sendFromEsmeNamed)
+			sendChannel <- messageToSendDescriptor
 		}
 	}
 }
 
-func parseCommandLine() (string, error) {
-	return "", nil
+type esmeClusterApplication struct {
+}
+
+func newEsmeClusterApplication() *esmeClusterApplication {
+	return &esmeClusterApplication{}
+}
+
+func (app *esmeClusterApplication) parseCommandLine() (string, error) {
+	if len(os.Args) != 2 {
+		return "", errors.New(app.syntaxString())
+	}
+
+	return os.Args[1], nil
+}
+
+func (app *esmeClusterApplication) syntaxString() string {
+	return fmt.Sprintf("%s <yaml_file>", path.Base(os.Args[0]))
+}
+
+func (app *esmeClusterApplication) addEsmePeer(emse *esme) {
+
+}
+
+func (app *esmeClusterApplication) mapEsmeNameToItsReceiverChannel(esmeName string, messageChannel chan<- *messageDescriptor) {
+
+}
+
+func (app *esmeClusterApplication) retrieveOutgoingMessageChannelFromEsmeNamed(esmeName string) chan<- *messageDescriptor {
+	return nil
 }
